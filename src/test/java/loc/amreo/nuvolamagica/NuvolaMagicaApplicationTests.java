b650a1dee64bb5d77af05ef4ae17c77d9cedd09b
amreo <amreo@live.it>
2018-02-26 21:09:51 +0100

    Added initial files

---
+++ src/test/java/loc/amreo/nuvolamagica/NuvolaMagicaApplicationTests.java
@@ -0,0 +1,16 @@
+package loc.amreo.nuvolamagica;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NuvolaMagicaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
